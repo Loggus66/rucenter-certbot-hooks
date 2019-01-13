d59e11db34d855e3f2d20811f2291b71fcc526bb
root <root@localhost.localdomain>
2019-01-14 02:59:55 +0500

    Hooks. Currently certbot domain var isn't used.

---
+++ auth-hook.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source $(dirname "$0")/creds.txt
+
+#get token for all methods
+TOKEN=$(curl -s -X POST 'https://api.nic.ru/oauth/token' \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-d "grant_type=password&username=$USERNAME&password=$PASSWORD&scope=.%2B%3A%2Fdns-master%2Fservices%2F$DNSACCOUNT%2Fzones%2F$ZONE(%2F.%2B)%3F&client_id=$CLIENTID&client_secret=$SECRET" \
+| tr '"' ' ' | awk '{print $8}')
+echo $TOKEN | tee /tmp/.$ZONE-token
+
+# add _acme-challenge TXT record
+curl -s -X PUT "https://api.nic.ru/dns-master/services/$DNSACCOUNT/zones/$ZONE/records" \
+-H "Authorization: Bearer $TOKEN" \
+-d "
+<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<request>
+ <rr-list>
+  <rr>
+   <name>_acme-challenge.$ZONE.</name>
+   <ttl>$TTL</ttl>
+   <type>TXT</type>
+   <txt>
+   <string>$CERTBOT_VALIDATION</string>
+   </txt>
+  </rr>
+ </rr-list>
+</request>
+"
+# commit zone records
+curl -s -X POST "https://api.nic.ru/dns-master/services/$DNSACCOUNT/zones/$ZONE/commit" \
+-H "Authorization: Bearer $TOKEN"
+
+# sleep for the $TTL period of time plus a tad more, so certbot won't go checking records when they aren't there yet
+sleep $TTL
+sleep 20
+
---
+++ cleanup-hook.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source $(dirname "$0")/creds.txt
+
+test -f /tmp/.$ZONE-token || exit 0 
+TOKEN=$(cat /tmp/.$ZONE-token)
+# grep all existing TXT _acme-challenge record IDs and delete them
+curl -s "https://api.nic.ru/dns-master/services/$DNSACCOUNT/zones/$ZONE/records" \
+-H "Authorization: Bearer $TOKEN" \
+| grep _acme-challenge | grep TXT | tr '"' ' ' | tr '>' ' ' | tr '<' ' ' | awk '{print $3}'| \
+while read rrid
+do
+echo $rrid
+curl -s -X DELETE "https://api.nic.ru/dns-master/services/$DNSACCOUNT/zones/$ZONE/records/$rrid" \
+-H "Authorization: Bearer $TOKEN" 
+done
+
+# commit zone
+curl -s -X POST "https://api.nic.ru/dns-master/services/$DNSACCOUNT/zones/$ZONE/commit" \
+-H "Authorization: Bearer $TOKEN"
+
+# remove token
+test -f /tmp/.$ZONE-token && rm -f /tmp/.$ZONE-token
+
+
